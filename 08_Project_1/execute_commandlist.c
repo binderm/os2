881c48c8fc7ee33150e87566fa34f882c14f467c
mrclbndr <marcel.binder@mailbox.org>
2017-05-04 20:21:08 +0200

    input/output redirection

--- 08_Project_1/execute_commandlist.c
+++ 08_Project_1/execute_commandlist.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
-#include <stdio.h>
 #include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
 #include <sys/wait.h>
 #include "execute_commandlist.h"
 
@@ -11,8 +13,8 @@ int execute_command(command *);
 int execute_commandlist(commandlist *clist) {
 	int command_result = 0;
 	for (command *com = clist->head;
-		!(command_result || com == NULL);
-		com = com->next_one) {
+			!(command_result || com == NULL);
+			com = com->next_one) {
 		command_result = execute_command(com);
 	}
 	return command_result;
@@ -29,13 +31,40 @@ int execute_command(command *com) {
 	}
 	argv[1 + argc] = NULL;
 
+	int in = STDIN_FILENO, out = STDOUT_FILENO;
+	if (com->in != NULL && (in = open(com->in, O_RDWR)) < 0) {
+		perror("Failed to open file for input redirection.");
+		return -1;
+	}
+	if (com->out != NULL && (out = open(com->out, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR)) < 0) {
+		perror("Failed to open file for output redirection.");
+		return -1;
+	}
+
 	pid_t child_pid = fork();
 	if (child_pid == 0) {
+		if (in != STDIN_FILENO && dup2(in, STDIN_FILENO) != STDIN_FILENO) {
+			perror("Failed to redirect input from file");
+			return -1;
+		}
+		if (out != STDOUT_FILENO && dup2(out, STDOUT_FILENO) != STDOUT_FILENO) {
+			perror("Failed to redirect output to file");
+			return -1;
+		}
 		execvp(file, argv);
 		perror("Failed to exec");
 		return -1;
 	} else if (child_pid < 0) {
 		perror("Failed to fork");
+		return -1;
+	}
+
+	if (in != STDIN_FILENO && close(in)) {
+		perror("Failed to close parent copy of file for input redirection");
+		return -1;
+	}
+	if (out != STDOUT_FILENO && close(out)) {
+		perror("Failed to close parent copy of file for output redirection");
 		return -1;
 	}
 
