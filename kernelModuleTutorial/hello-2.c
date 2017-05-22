9fe42fe5e9128a4860b33801245d2988d9a48b15
mrclbndr <marcel.binder@mailbox.org>
2017-05-22 21:12:07 +0200

    header guards

---
+++ kernelModuleTutorial/hello-2.c
@@ -0,0 +1,17 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+static int a_number __initdata = 42;
+
+static int __init set_up(void) {
+	printk(KERN_INFO "Hello world!\na_number=%d\n", a_number);
+	return 0;
+}
+
+static void __exit tear_down(void) {
+	printk(KERN_INFO "Goodbye world!\n");
+}
+
+module_init(set_up);
+module_exit(tear_down);
