f6079063b965090aee7012684becc6fd842a2379
mrclbndr <marcel.binder@mailbox.org>
2017-05-22 21:35:09 +0200

    defined supported ioctl commands and passed invocations to separate subroutines

--- 09_Project_2/src/ofs.c
+++ 09_Project_2/src/ofs.c
@@ -12,7 +12,36 @@ static ssize_t ofs_read(struct file *flip, char __user *buffer, size_t length, l
 	return 0;
 }
 
-static long ofs_ioctl(struct file *flip, unsigned int cmd, unsigned long arg) {
+static long ofs_find_files_opened_by_process(unsigned int pid) {
+	return 0;
+}
+
+static long ofs_find_files_opened_by_user(unsigned int uid) {
+	return 0;
+}
+
+static long ofs_find_open_files_owned_by_user(unsigned int uid) {
+	return 0;
+}
+
+static long ofs_find_open_files_with_name(char *filename) {
+	return 0;
+}
+
+static long ofs_ioctl(struct file *flip, unsigned int ioctl_cmd, unsigned long ioctl_arg) {
+	switch (ioctl_cmd) {
+		case OFS_PID:
+			return ofs_find_files_opened_by_process(*(unsigned int*) ioctl_arg);
+		case OFS_UID:
+			return ofs_find_files_opened_by_user(*(unsigned int *) ioctl_arg);
+		case OFS_OWNER:
+			return ofs_find_open_files_owned_by_user(*(unsigned int *) ioctl_arg);
+		case OFS_NAME:
+			return ofs_find_open_files_with_name((char *) ioctl_arg);
+		default:
+			printk(KERN_WARNING "Unknown ioctl command %ud\n", ioctl_cmd);
+			return EINVAL;
+	}
 	return 0;
 }
 
@@ -42,13 +71,13 @@ static int __init ofs_init(void) {
 	}
 
 	printk(KERN_INFO "ofs: Registered as character device under major number %d.\n" \
-		"Create special file via\n" \
-		"mknod /dev/openFileSearchDev c %d\n", major_num, major_num);
+			"Create special file via\n" \
+			"mknod /dev/openFileSearchDev c %d\n", major_num, major_num);
 	return 0;
 }
 
 static void __exit ofs_exit(void) {
-	printk(KERN_ERR "ofs: Successfully cleaned up OpenFileSearch device driver");
+	printk(KERN_ERR "ofs: Cleaned up");
 }
 
 module_init(ofs_init);
--- 09_Project_2/src/ofs.h
+++ 09_Project_2/src/ofs.h
@@ -3,4 +3,36 @@
 
 #include "ofs_result.h"
 
+/**
+ * ioctl command for findin all a process's open() files.
+ *
+ * ioctrl argument: unsigned int
+ *   id of process
+ */
+#define OFS_PID 0
+
+/**
+ * ioctl command for finding all the files a user has open().
+ *
+ * ioctl argument: unsigned int
+ *   id of user
+ */
+#define OFS_UID 1
+
+/**
+ * ioctl command for finding all open() files that are owned by a user.
+ *
+ * ioctl argument: unsigned int
+ *   id of user
+ */
+#define OFS_OWNER 2
+
+/**
+ * ioctl command for finding all files with a given name.
+ *
+ * ioctl argument: char* up to 64 bytes (null-terminated)
+ *   filename
+ */
+#define OFS_NAME 3
+
 #endif
