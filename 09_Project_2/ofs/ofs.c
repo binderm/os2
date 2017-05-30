4b8d8aa59351ad7a8c74ad9179203015930a1948
mrclbndr <marcel.binder@mailbox.org>
2017-05-31 01:20:10 +0200

    comments, aquire RCU lock for read access to fdtable

--- 09_Project_2/ofs/ofs.c
+++ 09_Project_2/ofs/ofs.c
@@ -34,9 +34,13 @@ static long ofs_find_files_opened_by_process(unsigned int pid) {
 	pid_t nr;
 	struct task_struct *task;
 	struct fdtable *fdt;
-	unsigned int max_fds;
-	int open_fds_index;
-	unsigned long open_fds;
+	unsigned int fd_capacity;
+	unsigned int fds_length;
+	unsigned int fds_section_index;
+	unsigned long open_fds_section;
+	unsigned int fds_bit_index;
+	unsigned int open_fd_index;
+	/*unsigned long open_fds;
 	int bit;
 	int fd_index;
 	struct file *fd;
@@ -45,8 +49,7 @@ static long ofs_find_files_opened_by_process(unsigned int pid) {
 	char *name;
 	struct inode *ino;
 	umode_t mode;
-	uid_t owner;
-
+	uid_t owner;*/
 
 	printk(KERN_INFO "ofs: Find open files of process %u\n", pid);
 	// wrap numberic pid to struct pid --> new struct pid is allocated when pid is reused (wrap around) --> safer reference
@@ -55,15 +58,48 @@ static long ofs_find_files_opened_by_process(unsigned int pid) {
 
 	task = get_pid_task(p, PIDTYPE_PID);
 	if (!task) {
-		printk(KERN_WARNING "ofs: Failed to get task_stuct for pid\n");
+		printk(KERN_WARNING "ofs: Failed to get task_stuct for pid %u. Might not exist.\n", pid);
 		return -EINVAL;
 	}
 	
-	fdt = task->files->fdt;
-	// maximum number of fd that can be hold in the current version of the fdtable
-	max_fds = fdt->max_fds;
+	// Reading the fdtable must be protected with RCU read lock
+	rcu_read_lock();
+	
+	// reference to file_struct::fdt MUST be done via files_fdtable macro
+	// --> ensures lock-free dereference (see <kernel>/Documentation/filesystems/files.txt)
+	// (files_struct::fdt initially points to files_struct::fdtab but elsewhere after possible expansion of fdtable)
+	fdt = files_fdtable(task->files);
+	// now one CAN safely read fdt
 
-	for (open_fds_index = 0; open_fds_index < max_fds; open_fds_index++) {
+	// fdtable primarily is a wrapper around a array of file descriptors (fdtable::fd of type struct file *)
+	// max_fds contains the capacity (not number of elements) of this array
+	// initially the capacity MUST be at least BITS_PER_LONG
+	// the fill state of the array CAN be determined via so called file descriptor sets (fdtable::xx_fds) = array of long values
+	// --> chained bit mask first bit of first long is for the first fd in the array, second bit for the second, ..., first bit of second long is for the (BITS_PER_LONG + 1)-th fd in the array
+	// length of the fds arrays is therefore max_fds / BITS_PER_LONG
+	fd_capacity = fdt->max_fds;
+	fds_length = fd_capacity / BITS_PER_LONG;
+	printk(KERN_DEBUG "ofs: fd_capacity=%u, fds_length=%u\n", fd_capacity, fds_length);
+
+	// e.g. opening a owned file in vim and write
+	// open_fds bits 1-3 (stdin, stdout, stderr) and 5 (.swp file for permanent backup) set = opened
+	// open_fds bits 4 (original file) not opened (only for a few moments when originally loading the files content)
+	// open_fds bits 6 (original file) used for writing (only opened for write operation)
+	// close_on_exec bit 3 set (.swp file) should be closed when changing process image but stdin, stdout, stderr should remain open of course
+	// TODO whats full_fds_bits for? -> always 0? length?
+	for (fds_section_index = 0; fds_section_index < fds_length; fds_section_index++) {
+		open_fds_section = fdt->open_fds[fds_section_index];
+		printk(KERN_DEBUG "ofs: open_fds[%u]=%lu\n", fds_section_index, open_fds_section);
+		
+		for (fds_bit_index = 0; fds_bit_index < BITS_PER_LONG; fds_bit_index++) {
+			if (open_fds_section & (1UL << fds_bit_index)) {
+				open_fd_index = fds_section_index + fds_bit_index;
+				printk(KERN_DEBUG "ofs: fd[%u] open\n", open_fd_index);
+			}
+		}
+	}
+
+	/*for (open_fds_index = 0; open_fds_index < max_fds; open_fds_index++) {
 		open_fds = fdt->open_fds[open_fds_index];
 		if (!open_fds) {
 			break;
@@ -82,7 +118,10 @@ static long ofs_find_files_opened_by_process(unsigned int pid) {
 				printk(KERN_INFO "ofs: %s\n     mode=%d\n     \nowner=%u\n", name, mode, owner);
 			}
 		}
-	}
+	}*/
+
+	// Unlock read access to fdtable
+	rcu_read_unlock();
 
 	return 0;
 }
