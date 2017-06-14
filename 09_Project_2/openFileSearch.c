45c49b634c153f949fdb016d909d18409e8216d4
mrclbndr <marcel.binder@mailbox.org>
2017-06-14 22:35:44 +0200

    separated search in task method

--- 09_Project_2/openFileSearch.c
+++ 09_Project_2/openFileSearch.c
@@ -39,7 +39,7 @@ static int ofs_open(struct inode *inode, struct file *flip) {
 		return -1;
 	}
 
-	// close any opened search performed by another caller
+	// discard search performed by prior caller
 	search_performed_ = 0;
 
 	printk(KERN_INFO "openFileSearch: Opened\n");
@@ -75,30 +75,84 @@ int ofs_filter_by_name(struct ofs_result *result, void *filter_arg) {
 	return strncmp(result->name, name, OFS_RESULT_NAME_MAX_LENGTH) == 0;
 }
 
-static void ofs_find_open_files_of_task(struct task_struct *task,
-		ofs_result_filter filter,
-		void *filter_arg) {
-	struct files_struct *files;
-	struct fdtable *fdt;
-	unsigned int fd_capacity;
-	unsigned int fds_length;
-	int limit_exerceeded;
-	struct ofs_result *result;
+static void ofs_create_result(pid_t pid, uid_t uid, struct file *open_file,
+ofs_result_filter filter, void *filter_arg) {
+	struct ofs_result *result = &results_[result_count_];
+	char *result_name_buffer;
+	char *result_name;
+	struct inode *inode;
+
+	result->pid = pid;
+	result->uid = uid;
+	
+	if (!(result_name_buffer = kmalloc(OFS_RESULT_NAME_MAX_LENGTH * sizeof(char), GFP_KERNEL))) {
+		printk(KERN_ERR "openFileSearch: Failed to allocate memory for result name\n");
+		return;
+	}
+	// TODO is atomic_long_inc_not_zero required on file->f_count?
+	result_name = d_path(&(open_file->f_path), result_name_buffer, OFS_RESULT_NAME_MAX_LENGTH);	
+	if (IS_ERR(result_name)) {
+		printk(KERN_WARNING "openFileSearch: Failed to get name for open file. Error code: %ld\n", PTR_ERR(result_name));
+		return; // TODO
+	}
+	strncpy(result->name, result_name, OFS_RESULT_NAME_MAX_LENGTH);
+	kfree(result_name_buffer);
+
+	inode = open_file->f_inode;
+	result->permissions = inode->i_mode;
+	result->owner = (inode->i_uid).val;
+	result->fsize = inode->i_size;
+	result->inode_no = inode->i_ino;
+
+	if (filter(result, filter_arg)) {
+		result_count_++;
+	}
+}
+
+static void ofs_search_in_fdt(pid_t pid, uid_t uid, struct fdtable *fdt,
+ofs_result_filter filter, void *filter_arg) {
+	// fdtable primarily is a wrapper around a array of file descriptors (fdtable::fd of type struct file *)
+	// max_fds contains the capacity (not number of elements) of this array
+	// initially the capacity MUST be at least BITS_PER_LONG
+	// the fill state of the array CAN be determined via so called file descriptor sets (fdtable::xx_fds) = array of long values
+	// --> chained bit mask first bit of first long is for the first fd in the array, second bit for the second, ..., first bit of second long is for the (BITS_PER_LONG + 1)-th fd in the array
+	// length of the fds arrays is therefore max_fds / BITS_PER_LONG
+	unsigned int fd_capacity = fdt->max_fds;
+	unsigned int fds_length = fd_capacity / BITS_PER_LONG;
+
 	unsigned int fds_section_index;
 	unsigned long open_fds_section;
 	unsigned int fds_bit_index;
 	unsigned int open_fd_index;
 	struct file *open_file;
-	char *result_name_buffer;
-	char *result_name;
-	struct inode *inode;
 
-	if (!(result_name_buffer = kmalloc(OFS_RESULT_NAME_MAX_LENGTH * sizeof(char), GFP_KERNEL))) {
-		printk(KERN_ERR "openFileSearch: Failed to allocate memory for result name\n");
-		return;
+	for (fds_section_index = 0; result_count_ < OFS_MAX_RESULTS
+			&& fds_section_index < fds_length; fds_section_index++) {
+		open_fds_section = fdt->open_fds[fds_section_index];
+
+		for (fds_bit_index = 0; result_count_ < OFS_MAX_RESULTS
+				&& fds_bit_index < BITS_PER_LONG; fds_bit_index++) {
+			if (open_fds_section & (1UL << fds_bit_index)) {
+				open_fd_index = fds_section_index + fds_bit_index;
+				open_file = fdt->fd[open_fd_index];
+				
+				if (open_file) {
+					ofs_create_result(pid, uid, open_file, filter, filter_arg);
+				} else {
+					printk(KERN_WARNING "openFileSearch: Failed to query struct file with index %u\n", open_fd_index);
+				}
+			}
+		}
 	}
+}
 
-	files = task->files;
+static void ofs_search_in_task(struct task_struct *task,
+		ofs_result_filter filter,
+		void *filter_arg) {
+	struct files_struct *files = task->files;
+	struct fdtable *fdt;
+	pid_t pid = task->pid;
+	uid_t uid = task->cred->uid.val;	
 
 	// Reading the fdtable must be protected with RCU read lock
 	rcu_read_lock();
@@ -109,83 +163,21 @@ static void ofs_find_open_files_of_task(struct task_struct *task,
 	fdt = files_fdtable(files);
 	// now one CAN safely read fdt
 
-	// fdtable primarily is a wrapper around a array of file descriptors (fdtable::fd of type struct file *)
-	// max_fds contains the capacity (not number of elements) of this array
-	// initially the capacity MUST be at least BITS_PER_LONG
-	// the fill state of the array CAN be determined via so called file descriptor sets (fdtable::xx_fds) = array of long values
-	// --> chained bit mask first bit of first long is for the first fd in the array, second bit for the second, ..., first bit of second long is for the (BITS_PER_LONG + 1)-th fd in the array
-	// length of the fds arrays is therefore max_fds / BITS_PER_LONG
-	fd_capacity = fdt->max_fds;
-	fds_length = fd_capacity / BITS_PER_LONG;
-	// TODO printk(KERN_DEBUG "openFileSearch: fd_capacity=%u, fds_length=%u\n", fd_capacity, fds_length);
-
-	// e.g. opening a owned file in vim and write
-	// open_fds bits 1-3 (stdin, stdout, stderr) and 5 (.swp file for permanent backup) set = opened
-	// open_fds bits 4 (original file) not opened (only for a few moments when originally loading the files content)
-	// open_fds bits 6 (original file) used for writing (only opened for write operation)
-	// close_on_exec bit 3 set (.swp file) should be closed when changing process image but stdin, stdout, stderr should remain open of course
-	// TODO whats full_fds_bits for? -> always 0? length?
-	limit_exerceeded = 0;
-	for (fds_section_index = 0; !limit_exerceeded && fds_section_index < fds_length; fds_section_index++) {
-		open_fds_section = fdt->open_fds[fds_section_index];
-		// TODO printk(KERN_DEBUG "openFileSearch: open_fds[%u]=%lu\n", fds_section_index, open_fds_section);
-
-		for (fds_bit_index = 0; !limit_exerceeded && fds_bit_index < BITS_PER_LONG; fds_bit_index++) {
-			if (open_fds_section & (1UL << fds_bit_index)) {
-				if (result_count_ > OFS_MAX_RESULTS) {
-					printk(KERN_WARNING "Found more than %u results\n", OFS_MAX_RESULTS); // TODO cancel search if searching all tasks
-					limit_exerceeded = 1;
-					break; // TODO remove break
-				}
-				result = &results_[result_count_];
-
-				open_fd_index = fds_section_index + fds_bit_index;
-
-				// for getting a struct file from the fd array fcheck_files MUST be used holding the RCU read lock (see implementation: the last parameter is the index for the fd array)
-				open_file = fcheck_files(files, open_fd_index);
-				if (open_file) {
-					result->pid = task->pid;
-					// TODO is atomic_long_inc_not_zero required on file->f_count?
-					result_name = d_path(&(open_file->f_path), result_name_buffer, OFS_RESULT_NAME_MAX_LENGTH);	
-
-					if (IS_ERR(result_name)) {
-						printk(KERN_WARNING "openFileSearch: Failed to get name for open file. Error code: %ld\n", PTR_ERR(result_name));
-						continue;
-					}
-					strncpy(result->name, result_name, OFS_RESULT_NAME_MAX_LENGTH);
-
-					inode = open_file->f_inode;
-					result->permissions = inode->i_mode;
-					result->uid = task->cred->uid.val;
-					result->owner = (inode->i_uid).val; // TODO use uid_t from_kuid(user_namespace, kuid_t) instead
-					result->fsize = inode->i_size;
-					result->inode_no = inode->i_ino;
-					if (filter(result, filter_arg)) {
-						// TODO printk(KERN_DEBUG "openFileSearch: Result#%u: name=%s\npermissions=%u, owner=%u, fsize=%u, inode_no=%lu\n", result_count_, result->name, result->permissions, result->owner, result->fsize, result->inode_no);
-						result_count_++;
-					}
-				} else {
-					printk(KERN_WARNING "openFileSearch: Failed to query struct file with index %u\n", open_fd_index);
-				}
-			}
-		}
-	}
+	ofs_search_in_fdt(pid, uid, fdt, filter, filter_arg);
 
 	// Unlock read access to fdtable
 	rcu_read_unlock();
-
-	kfree(result_name_buffer);
 }
 
-static void ofs_find_open_files_of_tasks(struct task_struct *task,
+static void ofs_search_in_children(struct task_struct *task,
 		ofs_result_filter filter, void *filter_arg) {
 	struct list_head *cursor;
 	struct task_struct *child;
 
-	ofs_find_open_files_of_task(task, filter, filter_arg);
+	ofs_search_in_task(task, filter, filter_arg);
 	list_for_each(cursor, &(task->children)) {
 		child = list_entry(cursor, struct task_struct, sibling);
-		ofs_find_open_files_of_tasks(child, filter, filter_arg);
+		ofs_search_in_children(child, filter, filter_arg);
 	}
 }
 
@@ -210,7 +202,7 @@ static long ofs_find_files_opened_by_process(pid_t requested_pid) {
 	}
 
 	new_search();
-	ofs_find_open_files_of_task(task, &ofs_no_filter, NULL);
+	ofs_search_in_task(task, &ofs_no_filter, NULL);
 	search_performed_ = 1;
 
 	printk(KERN_INFO "openFileSearch: %d results found\n", result_count_);
@@ -226,7 +218,7 @@ static long ofs_find_files_opened_by_user(unsigned int uid) {
 	// it is scheduled when a processor is idle... and its the father of all
 	// tasks --> use it as entry point for traversing all tasks
 	new_search();
-	ofs_find_open_files_of_tasks(&init_task, &ofs_filter_by_uid, &uid);
+	ofs_search_in_children(&init_task, &ofs_filter_by_uid, &uid);
 	search_performed_ = 1;
 
 	printk(KERN_INFO "openFileSearch: %d results found\n", result_count_);
@@ -238,7 +230,7 @@ static long ofs_find_open_files_owned_by_user(unsigned int owner) {
 			"user %u\n", owner);
 
 	new_search();
-	ofs_find_open_files_of_tasks(&init_task, &ofs_filter_by_owner, &owner);
+	ofs_search_in_children(&init_task, &ofs_filter_by_owner, &owner);
 	search_performed_ = 1;
 
 	printk(KERN_INFO "openFileSearch: %d results found\n", result_count_); 
@@ -250,7 +242,7 @@ static long ofs_find_open_files_with_name(char *filename) {
 			filename);
 
 	new_search();
-	ofs_find_open_files_of_tasks(&init_task, &ofs_filter_by_name, filename);
+	ofs_search_in_children(&init_task, &ofs_filter_by_name, filename);
 	search_performed_ = 1;
 
 	printk(KERN_INFO "openFileSearch: %d results found\n", result_count_);
@@ -285,8 +277,8 @@ static inline unsigned int ofs_min(unsigned int a, unsigned int b) {
 // ssize_t = long int, size_t = unsigned long, loff_t = long long 
 static ssize_t ofs_read(struct file *flip, char __user *buffer,
 		size_t requested_results, loff_t *offset) {
-	unsigned int available_results,
-	read_results;
+	unsigned int available_results;
+	unsigned int read_results;
 
 	printk(KERN_INFO "openFileSearch: Read request for %lu results\n",
 			requested_results);
@@ -336,7 +328,6 @@ static int __init ofs_init(void) {
 		return -1;
 	}
 
-	// log major number so user can create a device file
 	printk(KERN_INFO "openFileSearch: Registered as character device " \
 			"under major number %d.\nCreate a device file via\n" \
 			"mknod /dev/openFileSearchDev c %d 0\n",
@@ -346,7 +337,7 @@ static int __init ofs_init(void) {
 
 static void __exit ofs_exit(void) {
 	unregister_chrdev(major_num_, MODULE_NAME);
-	
+
 	printk(KERN_INFO "openFileSearch: Unregistered character device with " \
 			"major number %d\n", major_num_);
 }
